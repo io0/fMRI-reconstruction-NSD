7132dbce550ec91be6253c2f3dfbaa96cf7edb2d
io0 <marleyxiong0@gmail.com>
2023-12-09 01:42:42 +0000

    add script to download betas

--- src/setup.sh
+++ src/setup.sh
@@ -17,3 +17,13 @@ pip install pytorch-msssim
 mkdir ../train_logs/models
 wget https://dl.fbaipublicfiles.com/vicregl/convnext_xlarge_alpha0.75_fullckpt.pth
 wget https://huggingface.co/datasets/pscotti/naturalscenesdataset/resolve/main/sd_image_var_autoenc.pth
+
+mkdir -p nsddata_betas/ppdata/subj01/func1pt8mm/betas_fithrf
+cd nsddata_betas/ppdata/subj01/func1pt8mm/betas_fithrf
+for i in {0..40}
+do
+   # Add leading zeros to the session number
+   session_number=$(printf "%02d" $i)
+   
+   wget https://natural-scenes-dataset.s3.amazonaws.com/nsddata_betas/ppdata/subj01/func1pt8mm/betas_fithrf/betas_session${session_number}.nii.gz
+done
